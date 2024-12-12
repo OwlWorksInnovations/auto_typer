38dfd1e1f3762e5c5f3f4345ecdc809ea2a9d8da
owlworksinnovations <juanpistoris01@gmail.com>
2024-12-12 21:43:31 +0200

    Added c# version
    not sure if its faster but i prefer this one

---
+++ csharp/auto_typer/AutoTyper.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace auto_typer
+{
+    partial class AutoTyper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.typeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.inputBox.Location = new System.Drawing.Point(12, 12);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(776, 397);
+            this.inputBox.TabIndex = 0;
+            this.inputBox.TextChanged += new System.EventHandler(this.inputBox_TextChanged);
+            // 
+            // typeBtn
+            // 
+            this.typeBtn.Location = new System.Drawing.Point(375, 415);
+            this.typeBtn.Name = "typeBtn";
+            this.typeBtn.Size = new System.Drawing.Size(75, 23);
+            this.typeBtn.TabIndex = 1;
+            this.typeBtn.Text = "Type";
+            this.typeBtn.UseVisualStyleBackColor = true;
+            this.typeBtn.Click += new System.EventHandler(this.typeBtn_Click);
+            // 
+            // AutoTyper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.typeBtn);
+            this.Controls.Add(this.inputBox);
+            this.Name = "AutoTyper";
+            this.Text = "Auto Typer";
+            this.Load += new System.EventHandler(this.AutoTyper_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button typeBtn;
+    }
+}
+
---
+++ csharp/auto_typer/AutoTyper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace auto_typer
+{
+    public partial class AutoTyper : Form
+    {
+        public AutoTyper()
+        {
+            InitializeComponent();
+        }
+
+        private void AutoTyper_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputBox_TextChanged(object sender, EventArgs e)
+        {
+   
+        }
+
+        private async void typeBtn_Click(object sender, EventArgs e)
+        {
+            string inputBoxText = inputBox.Text;
+
+            if (string.IsNullOrEmpty(inputBoxText))
+            {
+                MessageBox.Show("Please enter some text to type.");
+                return;
+            }
+
+            typeBtn.Enabled = false;
+
+            try
+            {
+                await Task.Delay(5000);
+
+                SendKeys.SendWait(inputBoxText);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                typeBtn.Enabled = true;
+            }
+        }
+    }
+}
