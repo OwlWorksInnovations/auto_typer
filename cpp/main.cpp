3fee162a7a7104817ca5af38858d6a50e52bff9f
Juan <juanytfortnite@gmail.com>
2025-01-16 11:20:52 +0200

    Working on a c++ version

---
+++ cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string text;
+    cout << "Enter your text: ";
+    cin >> text;
+    cout << endl;
+
+    // Type text here
+}
